df2c17fd1abfb58740d41a9629090f5a08d16edc
Valery <valefourin@gmail.com>
2021-07-12 17:41:45 +0300

    remove unused var

--- public/js/main.js
+++ public/js/main.js
@@ -9,27 +9,22 @@ window.onload = function () {
       document.body.classList.remove('loaded_hiding');
     }, 500);
 
-
-    // const pushUrl = (href) => {
-    //     window.history.pushState({}, '', href);
-    //     window.dispatchEvent(new Event('popstate'));
-    // };
-
     // parallax on move
-    document.addEventListener("mousemove", parallax);
-    console.log(window.location.pathname)
     const frontImg = document.querySelector("#front-image");
-    console.log(frontImg)
-    function parallax(e) {
-        let _w = window.innerWidth/2;
-        let _h = window.innerHeight/2;
-        let _mouseX = e.clientX;
-        let _mouseY = e.clientY;
-        let _depth1 = `${50 - (_mouseX - _w) * 0.01}% ${50 - (_mouseY - _h) * 0.06}%`;
-        let _depth2 = `${50 - (_mouseX - _w) * 0.01}% ${50 - (_mouseY - _h) * 0.06}%`;
-        let _depth3 = `${50 - (_mouseX - _w) * 0.01}% ${50 - (_mouseY - _h) * 0.06}%`;
-        let x = `${_depth3}, ${_depth2}, ${_depth1}`;
-        // console.log(x);
-        frontImg.style.backgroundPosition = x;
+    if (frontImg) {
+        document.addEventListener("mousemove", parallax);
+        function parallax(e) {
+            let _w = window.innerWidth/2;
+            let _h = window.innerHeight/2;
+            let _mouseX = e.clientX;
+            let _mouseY = e.clientY;
+            let _depth1 = `${50 - (_mouseX - _w) * 0.01}% ${50 - (_mouseY - _h) * 0.06}%`;
+            let _depth2 = `${50 - (_mouseX - _w) * 0.01}% ${50 - (_mouseY - _h) * 0.06}%`;
+            let _depth3 = `${50 - (_mouseX - _w) * 0.01}% ${50 - (_mouseY - _h) * 0.06}%`;
+            let x = `${_depth3}, ${_depth2}, ${_depth1}`;
+            // console.log(x);
+            frontImg.style.backgroundPosition = x;
+        }
     }
+    
   }
--- src/App.js
+++ src/App.js
@@ -2,23 +2,13 @@
 // import logo from './logo.jpg';
 import './App.css';
 import AppRouter from './components/AppRouter';
-import {BrowserRouter, useHistory, useLocation} from 'react-router-dom';
-// import { observer } from 'mobx-react-lite';
+import {BrowserRouter} from 'react-router-dom';
 import NavMenu from './components/NavMenu';
 import Footer from './components/Footer';
 import Preloader from './components/Preloader';
-import { useEffect } from 'react';
 
 
 const App = () => {
-
-  // const history = useHistory() 
-
-  //  useEffect(() => {
-  //     history.listen((location) => { 
-  //        console.log(`You changed the page to: ${location.pathname}`) 
-  //     }) 
-  //  },[history]) 
 
   return (
     <BrowserRouter>
