91caaaca6a923eccdfb21e7a87f4343322723e0e
Valery <valefourin@gmail.com>
2021-07-14 21:33:59 +0300

    contact form action fix

--- src/components/home/ContactForm.js
+++ src/components/home/ContactForm.js
@@ -7,12 +7,16 @@ const ContactForm = (props) => {
     const { t } = useTranslation();
     return (
         <Container className={props.className + " contact-form"}>
-            <form className="needs-validation" action="#" method="POST" >
+            <form 
+                className="needs-validation" 
+                action="https://formspree.io/f/mbjqayjq" 
+                method="POST" 
+            >
                 <div className={"form-inner " + props.className}>
                     <h2 className="section-subtitle">{t('form.title')}</h2>
                     <input className="form-control" id="validationCustom01" type="text" name="name" placeholder={t('form.name')} required />
                     <input className="form-control" id="validationCustom02" type="email" name="_replyto" placeholder="Email" required />
-                    <input className="form-control" id="validationCustom03" type="number" name="_replyto" placeholder="WhatsApp" required />
+                    <input className="form-control" id="validationCustom03" type="number" placeholder="WhatsApp" />
                     <textarea name="message" id="validationCustom04" className="form-control" placeholder={t('form.msg')} rows="5" required></textarea>
                     <Btn type="submit" className="white" text={t('form.send')} variant="dark"/>
                 </div>
